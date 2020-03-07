be762a6413bcd7ba749aa1ddc8234bcce1605a2e
Angga Gantira <angga.gantira@gmail.com>
2020-03-08 02:34:11 +0700

    Add Middleware Role & Permission

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,12 @@ Auth::routes();
 Route::group(['middleware' => ['auth']], function() {
     Route::get('/home', 'HomeController@index')->name('home');
     
-    Route::resource('colour', 'ColourController');
-    Route::resource('type', 'TypeController');
-    Route::resource('size', 'SizeController');
-    Route::resource('product', 'ProductController');
-    Route::resource('warehouse', 'WarehouseController');
-    Route::resource('permission', 'PermissionController');
-    Route::resource('role', 'RoleController');
-    Route::resource('user', 'UserController');
+    Route::resource('colour', 'ColourController')->middleware(['permission:master']);
+    Route::resource('type', 'TypeController')->middleware(['permission:master']);
+    Route::resource('size', 'SizeController')->middleware(['permission:master']);
+    Route::resource('product', 'ProductController')->middleware(['permission:product']);
+    Route::resource('warehouse', 'WarehouseController')->middleware(['permission:warehouse']);
+    Route::resource('permission', 'PermissionController')->middleware(['permission:master']);
+    Route::resource('role', 'RoleController')->middleware(['permission:master']);
+    Route::resource('user', 'UserController')->middleware(['permission:user']);
 });
